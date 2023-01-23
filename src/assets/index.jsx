f19ff28d7f9524d3b89150f1e68809d0a1bf8ca5
Boris Tenkes <boris.tenkes@gmail.com>
2023-01-23 01:02:22 +0100

    Scss complete and added index file to assets

---
+++ src/assets/index.jsx
@@ -0,0 +1,41 @@
+import avatarAli from './avatar-ali.png';
+import avatarAnisha from './avatar-anisha.png';
+import avatarRichard from './avatar-richard.png';
+import avatarShanai from './avatar-shanai.png';
+import { ReactComponent as BgSimplifySectionDesktop } from './bg-simplify-section-desktop.svg';
+import { ReactComponent as BgSimplifySectionMobile } from './bg-simplify-section-mobile.svg';
+import { ReactComponent as BgTabletPattern } from './bg-tablet-pattern.svg';
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as iconClose } from './icon-close.svg';
+import { ReactComponent as iconFacebook } from './icon-facebook.svg';
+import { ReactComponent as iconHamburger } from './icon-hamburger.svg';
+import { ReactComponent as iconInstagram } from './icon-instagram.svg';
+import { ReactComponent as iconPinterest } from './icon-pinterest.svg';
+import { ReactComponent as iconTwitter } from './icon-twitter.svg';
+import { ReactComponent as iconYoutube } from './icon-youtube.svg';
+import { ReactComponent as illustrationIntro } from './illustration-intro.svg';
+
+export const avatars = {
+    ali: avatarAli,
+    anisha: avatarAnisha,
+    richard: avatarRichard,
+    shanai: avatarShanai,
+};
+
+export const icons = {
+    close: iconClose,
+    facebook: iconFacebook,
+    hamburger: iconHamburger,
+    instagram: iconInstagram,
+    pinterest: iconPinterest,
+    twitter: iconTwitter,
+    youtube: iconYoutube,
+};
+
+export default {
+    BgSimplifySectionDesktop,
+    BgSimplifySectionMobile,
+    BgTabletPattern,
+    Logo,
+    illustrationIntro,
+}
